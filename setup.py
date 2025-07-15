668f43889014af3c602ff5facf5c9287115cb22f
SebastianTrumbore <121204147+SebastianTrumbore@users.noreply.github.com>
2025-07-15 10:39:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,42 @@
 from setuptools import setup
 
 setup(
-    name = 'ASqrtC', 
-    version = '1.0.1',
-    description = 'ASC was made to be a fast and accurate way of calculating square root. It is 100% accurate and has no size limit.', 
-    py_modules = ["ASqrtC"],
-    package_dir = {'':'src'},
-   # packages = [''],
-    author = 'Sebastian Trumbore',
-    author_email = 'Trumbore.Sebastian@gmail.com',
-    long_description = open('README.md').read(),
-    long_description_content_type = "text/markdown",
+    name='ASqrtC',
+    version='2.0.0',
+    description=
+    ('ASqrtC is a fast and accurate square root calculator with no size limit. '
+     'It delivers exact results to any specified number of decimal places, '
+     'making it more precise and flexible than traditional methods. '
+     'ASqrtC outperforms Python’s built-in math and decimal modules in high-precision use cases.'
+     ),
+    py_modules=["ASqrtC"],
+    package_dir={'': 'src'},
+    author='Sebastian Trumbore',
+    author_email='Trumbore.Sebastian@gmail.com',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/SebastianTrumbore/ASqrtC',
     include_package_data=True,
-    classifiers  = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        "License :: OSI Approved :: BSD License",
+        'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
-        'Intended Audience :: Other Audience',
-        'Intended Audience :: Science/Research',
         'Intended Audience :: Education',
-        'Topic :: Text Processing',
-        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
         'Operating System :: OS Independent',
     ],
-    install_requires = [],
-    keywords = ['Square Root', 'Data Science', 'Exact', 'Sqrt', 'Accurate'],
-    
+    install_requires=[],
+    keywords=[
+        'square root', 'sqrt', 'math', 'numerical methods', 'accurate',
+        'precision', 'high precision', 'exact decimal', 'arbitrary precision',
+        'fast math', 'root calculator', 'big number math',
+        'scientific computing', 'math utilities', 'python3', 'educational',
+        'lightweight'
+    ],
 )
