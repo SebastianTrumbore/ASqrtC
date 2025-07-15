242e3ece972d6e523bb1b648d26110ddfc9a3c74
SebastianTrumbore <121204147+SebastianTrumbore@users.noreply.github.com>
2025-07-15 10:30:30 -0700

    Update ASqrtC.py

--- src/ASqrtC.py
+++ src/ASqrtC.py
@@ -3,52 +3,58 @@ __author_email__='Trumbore.Sebastian@gmail.com'
 
 
 def sqrt(number, decimal_length=2):
-    try:
-        float(number)
-        if number < 0:
-            print("\033[0;31m" + ">> Error, invalid number; number must be positive.")
-            return ""
-        i = 0
-        j = 0
-        broken_number = []
-        if len(str(number)) % 2 == 1 and "." not in str(number):
-            broken_number.append(str(number)[i])
-            i += 1
-        elif len(str(number)) % 2 == 0 and "." in str(number):
-            broken_number.append(str(number)[i])
-            i += 1
-        j = int(-(- (len(str(number)) - i / 2) // 1))
-        for counter in range(int(j)):
-            if not len(str(number)) < i + 1:
-                if "." in str(number)[i] + str(number)[i + 1]:
-                    broken_number.append(
-                        str(number)[i] + str(number)[i + 1] + str(number)[i + 2]
-                    )
-                    i += 3
-                else:
-                    broken_number.append(str(number)[i] + str(number)[i + 1])
-                    i += 2
-        q = ""
-        q2 = 0
-        number = ""
-        d_placement = len(broken_number)
-        for counter in range(len(broken_number) + int(decimal_length)):
-            if number == 0 and counter > len(broken_number):
-                pass
+    # Check if input is numeric
+    if not isinstance(number, (int, float)):
+        raise TypeError("\033[0;31mError: Input must be a number.")
+
+    # Check for negative values
+    if number < 0:
+        raise ValueError("\033[0;31mError: Number must be non-negative.")
+
+    # Sqrt calculations
+    i = 0
+    j = 0
+    broken_number = []
+
+    number_str = str(int(number)) if "." not in str(number) else str(number)
+    if len(number_str) % 2 == 1 and "." not in number_str:
+        broken_number.append(number_str[i])
+        i += 1
+    elif len(number_str) % 2 == 0 and "." in number_str:
+        broken_number.append(number_str[i])
+        i += 1
+
+    j = int(-(-(len(number_str) - i / 2) // 1))
+    for counter in range(int(j)):
+        if not len(number_str) < i + 1:
+            if "." in number_str[i:i + 2]:
+                broken_number.append(number_str[i:i + 3])
+                i += 3
             else:
-                if counter >= len(broken_number):
-                    number *= 100
-                else:
-                    number = round(int(str(number) + broken_number[counter]))
-                I = 0
-                while int(str(q2) + str(I)) * int(I) <= int(number):
-                    I += 1
-                I -= 1
-                number -= int(str(q2) + str(I)) * int(I)
-                q2 = int(q2) * 10 + int(I) * 2
-                q = str(q) + str(I)
-        answer = str(q[:d_placement:]) + "." + str(q[d_placement::])
-        return answer
-    except:
-        print("\033[0;31m" + ">> Error, invalid number")
-        return ""
+                broken_number.append(number_str[i:i + 2])
+                i += 2
+
+    q = ""
+    q2 = 0
+    number_int = 0
+    d_placement = len(broken_number)
+
+    for counter in range(len(broken_number) + int(decimal_length)):
+        if number_int == 0 and counter > len(broken_number):
+            pass
+        else:
+            if counter >= len(broken_number):
+                number_int *= 100
+            else:
+                number_int = int(str(number_int) + broken_number[counter])
+
+            I = 0
+            while int(str(q2) + str(I)) * I <= number_int:
+                I += 1
+            I -= 1
+            number_int -= int(str(q2) + str(I)) * I
+            q2 = int(q2) * 10 + I * 2
+            q += str(I)
+
+    answer = q[:d_placement] + "." + q[d_placement:]
+    return answer
